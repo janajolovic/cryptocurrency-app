f6b45bee178d0b8f562dde39dd00bd57a77d3b4d
Jana Jolovic <janajolovic04@gmail.com>
2022-03-29 13:08:14 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="navbar">
+
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+        
+      </div>
     </div>
   );
 }
