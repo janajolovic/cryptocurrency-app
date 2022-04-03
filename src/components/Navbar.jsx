a7839f84f575e872776f6e18329cc40dc8a93dc5
Jana Jolovic <janajolovic04@gmail.com>
2022-04-03 19:57:19 +0200

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Typography } from 'antd'
+import { Avatar, Button, Menu, Typography } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import icon from "../images/cryptocurrency.png"
@@ -12,6 +12,20 @@ const Navbar = () => {
                 <Link to="">Cryptocurrency app</Link>
             </Typography.Title>
         </div>
+        <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+                <Link to="/criptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
   )
 }
