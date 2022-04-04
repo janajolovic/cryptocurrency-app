46cb730fb09ff551d10db58bb2e742bef5f84ef2
Jana Jolovic <janajolovic04@gmail.com>
2022-04-04 20:52:56 +0200

    homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className='logo-container'>
             <Avatar src={icon} size="large"/>
             <Typography.Title level={2} className="logo">
-                <Link to="">Cryptocurrency</Link>
+                <Link to="/">CryptoApp</Link>
             </Typography.Title>
         </div>
         <Menu theme="dark">
