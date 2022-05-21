8127479754f5ce914fffc40c03bae0a69cec4051
Jana Jolovic <janajolovic04@gmail.com>
2022-05-21 17:53:53 +0200

    some changes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,45 +1,56 @@
-import React from 'react'
-import {useGetCryptoExchangesQuery} from '../services/cryptoApi'
-import { Col, Row, Typography, Select, Collapse } from 'antd';
-import millify from 'millify';
+// import React from 'react';
+// import millify from 'millify';
+// import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+// import HTMLReactParser from 'html-react-parser';
 
-const { Text } = Typography;
-const { Panel } = Collapse;
+// import { useGetExchangesQuery } from '../services/cryptoApi';
+// import Loader from './Loader';
 
-const Exchanges = () => {
-  const {data, isFetching} = useGetCryptoExchangesQuery();
-  const exchangesList = data?.data?.exhanges;
+// const { Text } = Typography;
+// const { Panel } = Collapse;
 
-  if (isFetching) return <Loader />
+// const Exchanges = () => {
+//   const { data, isFetching } = useGetExchangesQuery();
+//   const exchangesList = data?.data?.exchanges;
+//  // Note: To access this endpoint you need premium plan
+//   if (isFetching) return <Loader />;
 
-  return (
-    <>
-      <Row>
-        <Col span={6}>Exhanges</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
-        <Col span={6}>Change</Col>
-      </Row>
-      <Row>
-        {exchangesList?.map((exchange) => (
-          <Col span={24}>
-            <Collapse>
-              <Panel
-                key={exchange.id}
-                showArrow={false}
-                header={(
-                  <Row key={exchange.id}>
-                  </Row>
-                )}>
+//   return (
+//     <>
+//       <Row>
+//         <Col span={6}>Exchanges</Col>
+//         <Col span={6}>24h Trade Volume</Col>
+//         <Col span={6}>Markets</Col>
+//         <Col span={6}>Change</Col>
+//       </Row>
+//       <Row>
+//         {exchangesList.map((exchange) => (
+//           <Col span={24}>
+//             <Collapse>
+//               <Panel
+//                 key={exchange.uuid}
+//                 showArrow={false}
+//                 header={(
+//                   <Row key={exchange.uuid}>
+//                     <Col span={6}>
+//                       <Text><strong>{exchange.rank}.</strong></Text>
+//                       <Avatar className="exchange-image" src={exchange.iconUrl} />
+//                       <Text><strong>{exchange.name}</strong></Text>
+//                     </Col>
+//                     <Col span={6}>${millify(exchange.volume)}</Col>
+//                     <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+//                     <Col span={6}>{millify(exchange.marketShare)}%</Col>
+//                   </Row>
+//                   )}
+//               >
+//                 {HTMLReactParser(exchange.description || '')}
+//               </Panel>
+//             </Collapse>
+//           </Col>
+//         ))} 
+//       </Row>
+//     </>
+//   );
+// };
 
-              </Panel>
-            </Collapse>
-          </Col>
-        ))}
-      </Row>
-
-    </>
-  )
-}
-
-export default Exchanges
+// export default Exchanges;
