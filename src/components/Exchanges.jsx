3f8bb861c52f3824edccde70ae790b813ad782a4
Jana Jolovic <janajolovic04@gmail.com>
2022-05-21 17:20:58 +0200

    exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import {useGetCryptoExchangesQuery} from '../services/cryptoApi'
+import { Col, Row, Typography, Select, Collapse } from 'antd';
+import millify from 'millify';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
+  const {data, isFetching} = useGetCryptoExchangesQuery();
+  const exchangesList = data?.data?.exhanges;
+
+  if (isFetching) return 'Loading...';
+
   return (
-    <div>Exchanges</div>
+    <>
+      <Row>
+        <Col span={6}>Exhanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+      <Row>
+        {exchangesList?.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={(
+                  <Row key={exchange.id}>
+                  </Row>
+                )}>
+
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+
+    </>
   )
 }
 
